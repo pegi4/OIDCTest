a6cf792895d8e471abf611aa3cf061b91a48620b
Vito Senic <67893442+pegi4@users.noreply.github.com>
2024-10-28 17:16:55 +0100

    Big Bang

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import Fastify from 'fastify';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const server = Fastify({ logger: true });
+
+server.get('/', async (request, reply) => {
+  return { hello: 'world' };
+});
+
+const start = async () => {
+  try {
+    await server.listen({ port: 3000, host: '0.0.0.0' });
+    console.log('Server is running at http://localhost:3000');
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
